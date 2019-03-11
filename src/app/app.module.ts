9cdc7a5e03d8e24eb8dd94cab583fd9a8ad6b8fa
surekrishna <surekrrishna@gmail.com>
2019-03-11 23:27:21 +0530

    updated files after installed angular material cdk and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Material section
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //Material section
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
