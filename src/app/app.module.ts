263f9fe65c449cae504997dcd87d18a97d977c4b
surekrishna <surekrrishna@gmail.com>
2019-03-13 00:18:37 +0530

    added MatToolbarModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 //Material section
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
-import { AppComponent } from './app.component';
-import { CustomerListComponent } from './components/customers/customer-list/customer-list.component';
-import { OrderListComponent } from './components/orders/order-list/order-list.component';
-import { MessageListComponent } from './components/messages/message-list/message-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 //Routing section
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomerListComponent,
-    OrderListComponent,
-    MessageListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule,   
     //Material section
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,    
+    MatSidenavModule,   
+    MatToolbarModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
