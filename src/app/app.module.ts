df4719ebe1d744f266067c1018accc94fc66e5e8
surekrishna <surekrrishna@gmail.com>
2019-03-12 23:37:57 +0530

    restructed pacakges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,12 +24,12 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     //Material section
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule,
-    AppRoutingModule
+    MatSidenavModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
