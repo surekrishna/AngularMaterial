a5d2ae40cce202ce2166c3711dad39dab3f1060d
surekrishna <surekrrishna@gmail.com>
2019-03-12 23:05:06 +0530

    created app-routing.module manually and added customer,orders,messages routes and exported

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+    {
+        path : 'customers',
+        loadChildren : './customers/customers.module#CustomersModule'
+    },
+    {
+        path : 'messages',
+        loadChildren : './messages/messages.module#MessagesModule'
+    },
+    {
+        path : 'orders',
+        loadChildren : './orders/orders.module#OrdersModule'
+    },
+    {
+        path : '',
+        redirectTo : '',
+        pathMatch : 'full'
+    }
+];
+
+@NgModule({
+    imports : [ RouterModule.forRoot(routes) ],
+    exports : [ RouterModule ]
+})
+export class AppRoutingModule{}
