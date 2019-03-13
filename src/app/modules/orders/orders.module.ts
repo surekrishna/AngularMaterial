8ba7f6a174f6f88a036f40a6c20818b238f2d34f
surekrishna <surekrrishna@gmail.com>
2019-03-13 23:22:27 +0530

    added MatTabsModule and added three tabs orders,shipment and archived

--- src/app/modules/orders/orders.module.ts
+++ src/app/modules/orders/orders.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [ OrderListComponent ],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    MatTabsModule
   ]
 })
 export class OrdersModule { }
